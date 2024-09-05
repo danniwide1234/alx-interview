9089fd31f7bf9c452663c58820d98e194540ab57
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2024-09-05 12:55:35 +0100

    0x06-starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,28 @@
 #!/usr/bin/node
-const request = require('request');
-const filmId = process.argv[2];
-const requestOptions = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + filmId,
-  method: 'GET'
-};
+/* Prints all Casts of Star Wars movie
+Read the README.md file for more info
+*/
 
-request(requestOptions, function (err, res, data) {
-  if (!err) {
-    const characterUrls = JSON.parse(data).characters;
-    displayCharacterNames(characterUrls, 0);
-  }
+const request = require('request');
+const apiBaseUrl = 'https://swapi-api.alx-tools.com/api/';
+const filmsEndpoint = 'films/';
+const filmId = process.argv[2].toString();
+
+request(apiBaseUrl + filmsEndpoint + filmId, function (err, _, responseBody) {
+  if (err) console.error(err);
+  const filmData = JSON.parse(responseBody);
+  const characters = filmData.characters;
+  displayCharacterNames(characters);
 });
 
-function displayCharacterNames (urls, idx) {
-  request(urls[idx], function (err, res, data) {
-    if (!err) {
-      console.log(JSON.parse(data).name);
-      if (idx + 1 < urls.length) {
-        displayCharacterNames(urls, idx + 1);
-      }
+/* Recursively and synchronously request each character
+and print out the character names */
+function displayCharacterNames (characterUrls, index = 0) {
+  request(characterUrls[index], function (err, _, responseBody) {
+    if (err) console.error(err);
+    console.log(JSON.parse(responseBody).name);
+    if (++index < characterUrls.length) {
+      displayCharacterNames(characterUrls, index);
     }
   });
 }
