34c7032c13d20c04a7cfe27419917f9c6e59ca9f
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2024-09-05 12:51:16 +0100

    0x06-starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,56 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const filmId = process.argv[2];
-const filmUrl = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-let characterUrls = [];
-const characterNames = [];
+const requestOptions = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + filmId,
+  method: 'GET'
+};
 
-const fetchCharacterUrls = async () => {
-  return new Promise((resolve, reject) => {
-    request(filmUrl, (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        console.error('Error: ', error, '| StatusCode: ', response.statusCode);
-        reject();
-      } else {
-        const data = JSON.parse(body);
-        characterUrls = data.characters;
-        resolve();
+request(requestOptions, function (err, _, data) {
+  if (!err) {
+    const characterUrls = JSON.parse(data).characters;
+    displayCharacterNames(characterUrls, 0);
+  }
+});
+
+function displayCharacterNames (urls, idx) {
+  request(urls[idx], function (err, _, data) {
+    if (!err) {
+      console.log(JSON.parse(data).name);
+      if (idx + 1 < urls.length) {
+        displayCharacterNames(urls, idx + 1);
       }
-    });
-  });
-};
-
-const fetchCharacterNames = async () => {
-  if (characterUrls.length > 0) {
-    for (const url of characterUrls) {
-      await new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-          if (error || response.statusCode !== 200) {
-            console.error('Error: ', error, '| StatusCode: ', response.statusCode);
-            reject();
-          } else {
-            const data = JSON.parse(body);
-            characterNames.push(data.name);
-            resolve();
-          }
-        });
-      });
     }
-  } else {
-    console.error('Error: Got no Characters for some reason');
-  }
-};
-
-const displayCharacterNames = async () => {
-  await fetchCharacterUrls();
-  await fetchCharacterNames();
-
-  // Print each name on a new line
-  for (const name of characterNames) {
-    console.log(name);
-  }
-};
-
-displayCharacterNames();
+  });
+}
