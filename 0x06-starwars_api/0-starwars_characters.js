d6ff3fd8459f247ab99f301654d8ddb8705d11fc
Egbuluese Daniel Emesho <danniwide.1981@gmail.com>
2024-09-05 12:45:20 +0100

    0x06-starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,29 +8,35 @@ let characterUrls = [];
 const characterNames = [];
 
 const fetchCharacterUrls = async () => {
-  await new Promise(resolve => request(filmUrl, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      console.error('Error: ', error, '| StatusCode: ', response.statusCode);
-    } else {
-      const data = JSON.parse(body);
-      characterUrls = data.characters;
-      resolve();
-    }
-  }));
+  return new Promise((resolve, reject) => {
+    request(filmUrl, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.error('Error: ', error, '| StatusCode: ', response.statusCode);
+        reject();
+      } else {
+        const data = JSON.parse(body);
+        characterUrls = data.characters;
+        resolve();
+      }
+    });
+  });
 };
 
 const fetchCharacterNames = async () => {
   if (characterUrls.length > 0) {
     for (const url of characterUrls) {
-      await new Promise(resolve => request(url, (error, response, body) => {
-        if (error || response.statusCode !== 200) {
-          console.error('Error: ', error, '| StatusCode: ', response.statusCode);
-        } else {
-          const data = JSON.parse(body);
-          characterNames.push(data.name);
-          resolve();
-        }
-      }));
+      await new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+          if (error || response.statusCode !== 200) {
+            console.error('Error: ', error, '| StatusCode: ', response.statusCode);
+            reject();
+          } else {
+            const data = JSON.parse(body);
+            characterNames.push(data.name);
+            resolve();
+          }
+        });
+      });
     }
   } else {
     console.error('Error: Got no Characters for some reason');
@@ -41,12 +47,9 @@ const displayCharacterNames = async () => {
   await fetchCharacterUrls();
   await fetchCharacterNames();
 
+  // Print each name on a new line
   for (const name of characterNames) {
-    if (name === characterNames[characterNames.length - 1]) {
-      process.stdout.write(name);
-    } else {
-      process.stdout.write(name + '\n');
-    }
+    console.log(name);
   }
 };
 
